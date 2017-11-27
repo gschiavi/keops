6b20463fed4122adc56731b852539fa2ff99442f
alexandre <alexandre@katrid.com>
2017-11-27 11:13:21 -0200

    Initial commit

--- keops/static/api/1.5/data.js
+++ keops/static/api/1.5/data.js
@@ -447,7 +447,7 @@
 
     DataSource.prototype.newRecord = function() {
       this.setState(DataSourceState.inserting);
-      this.scope.record = {};
+      this.scope.record = null;
       return this.scope.model.getDefaults().done((function(_this) {
         return function(res) {
           if (res.result) {
