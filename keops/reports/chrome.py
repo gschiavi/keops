af38729742129c0af3d55efab16ed6b222032ca8
alexandre <alexandre@katrid.com>
2018-11-14 23:06:35 -0200

    Initial commit

--- keops/reports/chrome.py
+++ keops/reports/chrome.py
@@ -32,6 +32,7 @@ class ReportEngine:
 
     def render(self, **kwargs):
         def query(cmd, *args, **kwargs):
+            sql = kwargs.get('sql')
             with connection.cursor() as cur:
                 cur.execute(cmd, *args, **kwargs)
                 rows = cur.fetchall()
@@ -41,6 +42,30 @@ class ReportEngine:
         return self.report.render(select=query, **kwargs)
 
     def to_pdf(self, **kwargs):
+        data = kwargs.get('data')
+        # Get sql
+        sqls = ['1 = 1']
+
+        for param in data:
+            if 'value1' in param and param['value1']:
+                val1 = param['value1']
+                val2 = param.get('value2')
+                if param['type'] == 'DateTimeField':
+                    if val1:
+                        val1 = "TO_DATE('%s', 'yyyy-mm-dd')" % val1
+                    if val2:
+                        val2 = "TO_DATE('%s', 'yyyy-mm-dd')" % val2
+                if param['op'] == 'contains':
+                    sqls.append("upper({0}) like upper('%{1}%')".format(param['name'], param['value1']))
+                elif param['op'] == 'startsWith':
+                    sqls.append("upper({0}) like upper('{1}%')".format(param['name'], param['value1']))
+                elif param['op'] == 'equals':
+                    sqls.append("{0} = '{1}'".format(param['name'], param['value1']))
+                elif param['op'] == 'between':
+                    sqls.append("{0} BETWEEN {1} and {2}".format(param['name'], val1, val2))
+        sql = ' AND '.join(sqls)
+        kwargs['sql'] = sql
+
         xml = self.render(**kwargs)
         fname = uuid.uuid4().hex + '.html'
         file_path = os.path.join(settings.REPORT_ROOT, fname)
@@ -48,5 +73,5 @@ class ReportEngine:
         with open(file_path, 'wb') as tmp:
             tmp.write(xml)
             tmp.close()
-            subprocess.call([settings.CHROME_PATH, '--headless', '--disable-gpu', '--print-to-pdf=' + output_path, 'file://' + file_path])
+            subprocess.call([settings.CHROME_PATH, '--file=' + file_path, output_path, '--javascript'])
             return fname + '.pdf'
